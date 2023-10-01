c334223c26dcdd61f00eecd8b93db196705bf691
bopowers9 <bopowers9@gmail.com>
2023-10-01 17:50:47 -0400

    fixed unused vars

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,8 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, Html, Environment, useProgress } from '@react-three/drei'
+import { Html, Environment, useProgress } from '@react-three/drei'
 import { XR, VRButton, TeleportationPlane, Controllers } from '@react-three/xr'
-import { Box, Torus } from "@react-three/drei";
+import { Torus } from "@react-three/drei";
 import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
 
 function Loader() {
@@ -16,7 +16,7 @@ export default function Game() {
         <>
             <VRButton onError={(e) => console.error(e)} />
             <Canvas>
-                <color attach="background" args={['black']} />
+                <color attach="background" args={['gray']} />
                 <Suspense fallback={<Loader />}>
                     <Environment preset="sunset" background />
                     <ambientLight intensity={0.5} />
