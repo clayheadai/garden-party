8538bef6766b5ef4ed9acff1ec14f9674fa0eb8d
bopowers9 <bopowers9@gmail.com>
2023-09-30 23:04:07 -0400

    added model

--- src/Game.js
+++ src/Game.js
@@ -1,34 +1,25 @@
 import { Canvas } from '@react-three/fiber'
-import { Environment } from '@react-three/drei'
+import { useGLTF } from '@react-three/drei'
 import { XR, VRButton, TeleportationPlane, Controllers, RayGrab } from '@react-three/xr'
 
 export default function Game() {
-  return (
-    <>
-      <VRButton onError={(e) => console.error(e)} />
-      <Canvas>
-        <color attach="background" args={['black']} />
-        <Environment preset="city" background />
-        <XR>
-          <Controllers />
-          <TeleportationPlane leftHand />
-          <RayGrab>
-            <mesh position={[1, 0, 0]}>
-                <boxGeometry args={[0.1, 0.1, 0.1]} />
-                <meshBasicMaterial color="red" />
-            </mesh>
-          </RayGrab>
-          <mesh position={[0, 1, 0]}>
-            <boxGeometry args={[0.1, 0.1, 0.1]} />
-            <meshBasicMaterial color="green" />
-          </mesh>
-          <mesh position={[0, 0, 1]}>
-            <boxGeometry args={[0.1, 0.1, 0.1]} />
-            <meshBasicMaterial color="blue" />
-          </mesh>
-          <ambientLight intensity={0.5} />
-        </XR>
-      </Canvas>
-    </>
-  )
+    const { scene } = useGLTF("./models/ground.glb")
+    return (
+        <>
+            <VRButton onError={(e) => console.error(e)} />
+            <Canvas>
+                <color attach="background" args={['black']} />
+                <XR>
+                <Controllers />
+                <TeleportationPlane leftHand />
+                <group dispose={null}>
+                    <primitive object={scene}>
+                        <meshBasicMaterial color="red" />
+                    </primitive>
+                </group>
+                <ambientLight intensity={0.5} />
+                </XR>
+            </Canvas>
+        </>
+    )
 }
