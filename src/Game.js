29365d086e10b589c04904f26c361ed0f3daf905
bopowers9 <bopowers9@gmail.com>
2023-10-01 17:46:10 -0400

    physics

--- src/Game.js
+++ src/Game.js
@@ -1,25 +1,41 @@
+import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, Environment } from '@react-three/drei'
+import { useGLTF, Html, Environment, useProgress } from '@react-three/drei'
 import { XR, VRButton, TeleportationPlane, Controllers } from '@react-three/xr'
+import { Box, Torus } from "@react-three/drei";
+import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+
+function Loader() {
+    const { total } = useProgress()
+    return <Html center>{Math.round(total)} % loaded</Html>
+  }
 
 export default function Game() {
-    const { scene } = useGLTF("./models/ground.glb")
+    // const { scene } = useGLTF("./models/ground.glb")
     return (
         <>
             <VRButton onError={(e) => console.error(e)} />
             <Canvas>
                 <color attach="background" args={['black']} />
-                <XR>
-                <Controllers />
-                <TeleportationPlane leftHand />
-                <group dispose={null}>
-                    <primitive object={scene}>
-                        <meshBasicMaterial color="orange" wireframe />
-                    </primitive>
-                </group>
-                <Environment preset="sunset" background />
-                <ambientLight intensity={0.5} />
-                </XR>
+                <Suspense fallback={<Loader />}>
+                    <Environment preset="sunset" background />
+                    <ambientLight intensity={0.5} />
+                    <XR>
+                        <Controllers />
+                        <Physics debug>
+                            <RigidBody>
+                                <TeleportationPlane leftHand />
+                            </RigidBody>
+                            
+                            <RigidBody colliders={"hull"} restitution={2}>
+                                <Torus />
+                            </RigidBody>
+
+                            <CuboidCollider position={[0, -2, 0]} args={[20, 0.5, 20]} />
+                        </Physics>
+                    </XR>
+                </Suspense>
+                
             </Canvas>
         </>
     )
